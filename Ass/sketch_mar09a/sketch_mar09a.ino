19b13ad1c88fe87a4b8cec051b7e934c46170b08
Olav3ELDA1 <90253818+Olav3ELDA1@users.noreply.github.com>
2022-03-11 11:25:25 +0100

    Add files via upload

---
+++ Ass/sketch_mar09a/sketch_mar09a.ino
@@ -0,0 +1,29 @@
+#include <ESP8266WiFi.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(D5, OUTPUT);
+
+  Serial.begin(115200);
+  Serial.println();
+
+  WiFi.begin("data_iot", "elev1234");
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(D4, HIGH);
+  delay(500);
+  digitalWrite(D4, LOW);
+  delay(500);
+}
